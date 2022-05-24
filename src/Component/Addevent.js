a27d72d040ea45717d6e6367c305c07f0316d2ac
anilamurali <anilamurali98@gmail.com>
2022-05-24 21:50:37 +0530

    first commit

---
+++ src/Component/Addevent.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+const Addevent = () => {
+    var [name,setName]=useState("")
+    var [date,setDate]=useState("")
+    var [venue,setVenue]=useState("")
+    var [organizer,setOrganizer]=useState("")
+    var [contact,setContact]=useState("")
+    const setData=()=>{
+        var data={"name":name,"date":date,"vanue":venue,"organizer":organizer,"contact":contact}
+        console.log(data)
+    }
+  return (
+    <div><div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Event Name</label>
+                    <input onChange={(e)=>{setName(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Date</label>
+                    <input onChange={(e)=>{setData(e.target.value)}} type="date" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Venue</label>
+                    <input onChange={(e)=>{setVenue(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Organizer</label>
+                    <input onChange={(e)=>{setOrganizer(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Contact Number</label>
+                    <input onChange={(e)=>{setContact(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={setData} class="btn btn-success">SUBMIT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Addevent
